751704e6889d97c6a5e873242ef3db575d9ab2fb
HARPER Jon <jon.harper87@gmail.com>
2023-08-30 17:09:21 +0200

    Don't use groovy to generate javadoc
    This adds a package-info.class file to the generated jar but it shouldn't be a problem
    This is because maven uses the jar as the input to the javadoc tool and we don't
    want to diverge too much from the maven way (you get problems really fast)
    (In theory, I guess we could generated the javadoc from target/classes instead of target/project.jar)
    
    The generated javadoc has one package with one class because:
    - It doesn't seem to be possible to generate an empty javadoc (no packages).
    - It doesn't seem to be possible to generate an empty package (no classes).
    
    references:
    * https://stackoverflow.com/questions/53706943/how-to-upload-an-artifact-to-maven-central-with-an-empty-javadoc-jar-or-empty-s
     => what we did in 1.0.0 and 1.0.1: totally empty javadoc.jar (valid for maven central upload but ugly)
    * https://stackoverflow.com/questions/59071851/how-to-force-a-javadoc-jar-even-though-there-is-no-public-javadoc
     => still produces a totally empty javadoc.jar (and big scary red errors)
    * https://stackoverflow.com/questions/1138390/javadoc-for-package-info-java-only
     => This plus excluding the class from the jar seems like the best solution for now ?
    
    promising but not yet ready:
    * https://issues.apache.org/jira/browse/MJAVADOC-329
      https://github.com/apache/maven-javadoc-plugin/pull/65
    * https://bugs.openjdk.org/browse/JDK-8193107
      https://hg.openjdk.org/jdk/jdk/rev/111104f1e033
      empty modules are allowed, but does becoming a module work with how the native library are loaded ??
    
    Signed-off-by: HARPER Jon <jon.harper87@gmail.com>

---
+++ src/com/powsybl/mathnative/MathNativeJavadocPlaceholder.java
@@ -0,0 +1,4 @@
+package com.powsybl.mathnative;
+
+/** Placeholder needed by the javadoc tool to generate the javadoc site */
+public class MathNativeJavadocPlaceholder { private MathNativeJavadocPlaceholder() { } }
