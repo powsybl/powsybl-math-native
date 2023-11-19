d6e6b9fac3bdb7dff4195adefc63c2c7e7c490f6
Geoffroy Jamgotchian <geoffroy.jamgotchian@gmail.com>
2023-11-19 14:41:07 +0100

    Add Sundials KinSol solver
    Signed-off-by: Geoffroy Jamgotchian <geoffroy.jamgotchian@gmail.com>

---
+++ src/solver.cpp
@@ -0,0 +1,69 @@
+/**
+ * Copyright (c) 2023, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * @file solver.cpp
+ * @author Geoffroy Jamgotchian <geoffroy.jamgotchian at rte-france.com>
+ */
+
+#include <kinsol/kinsol.h>
+#include <sundials/sundials_nvector.h>
+#include <sunmatrix/sunmatrix_sparse.h>
+#include <sunlinsol/sunlinsol_klu.h>
+#include <nvector/nvector_serial.h>
+#include "jniwrapper.hpp"
+
+namespace powsybl {
+
+}
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+JNIEXPORT void JNICALL Java_com_powsybl_math_solver_KinsolSolver(JNIEnv * env, jobject) {
+    try {
+        SUNContext sunctx;
+        int error = SUNContext_Create(nullptr, &sunctx);
+        if (error != 0) {
+            throw std::runtime_error("SUNContext_Create error " + std::to_string(error));
+        }
+        // to solve
+        // x^2 + y^2 = 10
+        // xy - 3 = 0
+        int n = 2;
+        double x0_data[2] = {0, 0};
+        N_Vector x0 = N_VMake_Serial(n, x0_data, sunctx);
+
+        SUNMatrix j = SUNSparseMatrix(n, n, 4, CSC_MAT, sunctx);
+
+        SUNLinearSolver ls = SUNLinSol_KLU(x0, j, sunctx);
+        if (!ls) {
+            throw std::runtime_error("SUNLinSol_KLU error");
+        }
+
+        error = SUNLinSolFree_KLU(ls);
+        if (error != 0) {
+            throw std::runtime_error("SUNLinSolFree_KLU error " + std::to_string(error));
+        }
+
+        SUNMatDestroy(j);
+
+        N_VDestroy_Serial(x0);
+
+        error = SUNContext_Free(&sunctx);
+        if (error != 0) {
+            throw std::runtime_error("SUNContext_Create error " + std::to_string(error));
+        }
+    } catch (const std::exception& e) {
+        powsybl::jni::throwMatrixException(env, e.what());
+    } catch (...) {
+        powsybl::jni::throwMatrixException(env, "Unknown exception");
+    }
+}
+
+#ifdef __cplusplus
+}
+#endif
