5ff18050fc8e8d7951c2f77510db3bd4b5526a86
Geoffroy Jamgotchian <geoffroy.jamgotchian@gmail.com>
2023-11-22 00:29:57 +0100

    Wip
    Signed-off-by: Geoffroy Jamgotchian <geoffroy.jamgotchian@gmail.com>

--- src/test/java/com/powsybl/math/solver/NewtonKrylovSolverContext.java
+++ src/test/java/com/powsybl/math/solver/NewtonKrylovSolverContext.java
@@ -6,7 +6,6 @@
  */
 package com.powsybl.math.solver;
 
-import com.powsybl.math.matrix.SparseMatrix;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,6 +15,18 @@ import org.slf4j.LoggerFactory;
 public class NewtonKrylovSolverContext {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(NewtonKrylovSolverContext.class);
+
+    private final double[] x;
+    private final int[] ap;
+    private final int[] ai;
+    private final double[] ax;
+
+    public NewtonKrylovSolverContext(double[] x, int[] ap, int[] ai, double[] ax) {
+        this.x = x;
+        this.ap = ap;
+        this.ai = ai;
+        this.ax = ax;
+    }
 
     public void logError(int errorCode, String module, String function, String message) {
         LOGGER.error("KinSol error: code={}, module='{}', function='{}', message='{}'",
@@ -27,15 +38,33 @@ public class NewtonKrylovSolverContext {
                 module, function, message);
     }
 
-    public void updateX(double[] x) {
-
+    public void updateF(double[] f) {
+        // 0 = 0.02 + v2 * 0.1 * sin(ph2)
+        // 0 = 0.01 + v2 * 0.1 (-cos(ph2) + v2)
+        // solution: (0.855373, -0.236001)
+        double v2 = x[0];
+        double ph2 = x[1];
+        f[0] = 0.02 + v2 * 0.1 * Math.sin(ph2);
+        f[1] = 0.01 + v2 * 0.1 * (-Math.cos(ph2) + v2);
     }
 
-    public double[] evalF() {
-        return null;
-    }
-
-    public SparseMatrix evalJ() {
-        return null;
+    public void updateJ() {
+        double v2 = x[0];
+        double ph2 = x[1];
+        double dp2dv2 = 0.1 * Math.sin(ph2);
+        double dp2dph2 = v2 * 0.1 * Math.cos(ph2);
+        double dq2dv2 = - 0.1 * Math.cos(ph2) + 2 * v2 * 0.1;
+        double dq2dph2 = v2 * 0.1 * Math.sin(ph2);
+        ap[0] = 0;
+        ap[1] = 2;
+        ap[2] = 4;
+        ax[0] = dp2dv2;
+        ax[1] = dp2dph2;
+        ax[2] = dq2dv2;
+        ax[3] = dq2dph2;
+        ai[0] = 0;
+        ai[1] = 1;
+        ai[2] = 0;
+        ai[3] = 1;
     }
 }
